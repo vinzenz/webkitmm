fbb060b08b436f999e22ca42ccc6e7cafb72b4fa
Jonathon Jongsma <jonathon.jongsma@collabora.co.uk>
2008-06-23 22:50:50 -0500

    remove some more libgda source files

--- webkit/webkitmm/init.cc
+++ webkit/webkitmm/init.cc
@@ -1,9 +1,6 @@
-// -*- c++ -*-
-/* $Id: init.cc 622 2004-03-29 17:59:17Z murrayc $ */
-
 /* init.cc
  *
- * Copyright 2001      Free Software Foundation
+ * Copyright 2008 Jonathon Jongsma
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
@@ -20,26 +17,18 @@
  * Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
 
-#include <libgdamm/init.h>
 #include <glibmm/init.h>
-#include <libgdammconfig.h> //For LIBGDAMM_VERSION
-#include <libgdamm/wrap_init.h>
-#include <libgda/libgda.h>
+#include <webkitmm/wrap_init.h>
 
 
-namespace Gnome
+namespace WebKit
 {
 
-namespace Gda
+void init(int nargs, gchar *args[])
 {
-
-void init(const Glib::ustring& app_id, const Glib::ustring& version, int nargs, gchar *args[])
-{
-  Glib::init(); //Sets up the g type system and the Glib::wrap() table.
+  Glib::init(nargs, args); //Sets up the g type system and the Glib::wrap() table.
   wrap_init(); //Tells the Glib::wrap() table about the libgnomecanvasmm classes.
-  gda_init(app_id.c_str(), version.c_str(), nargs, args);
 }
 
 
-} //namespace Gda
-} //namespace Gnome
+} //namespace WebKit
--- webkit/webkitmm/init.h
+++ webkit/webkitmm/init.h
@@ -1,11 +1,6 @@
-// -*- c++ -*-
-#ifndef _LIBGDAMM_INIT_H
-#define _LIBGDAMM_INIT_H
-/* $Id: init.h 497 2003-10-12 09:50:16Z murrayc $ */
-
 /* init.h
  *
- * Copyright 2001      Free Software Foundation
+ * Copyright 2008 Jonathon Jongsma
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
@@ -22,19 +17,14 @@
  * Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
 
-#include <glibmm.h>
+#ifndef WEBKITMM_INIT_H
+#define WEBKITMM_INIT_H
 
-
-namespace Gnome
+namespace WebKit
 {
 
-namespace Gda
-{
+void init(int nargs, gchar *args[]);
 
-void init(const Glib::ustring& app_id, const Glib::ustring& version, int nargs, gchar *args[]);
+} //namespace WebKit
 
-} //namespace Gda
-} //namespace Gnome
-
-#endif //_LIBGDAMM_INIT_H
-
+#endif //WEBKITMM_INIT_H
